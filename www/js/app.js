3dedeb9585c7328136445e0926562ec47c569f78
William Rockwood <wrockwood@tadl.org>
2014-08-27 17:51:41 -0400

    placeholder images for errors
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- www/js/app.js
+++ www/js/app.js
@@ -651,3 +651,15 @@ app.directive('ngEnter', function () {
         });
     };
 });
+app.directive('errSrc', function() {
+    return {
+        link: function(scope, element, attrs) {
+            element.bind('error', function() {
+                if (attrs.src != attrs.errSrc) {
+                    attrs.$set('src', attrs.errSrc);
+                }
+            });
+        }
+    }
+});
+
