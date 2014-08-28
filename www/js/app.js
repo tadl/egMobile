7a192709b42923d1f258eaeeb8f39333c5b08cdb
William Rockwood <wrockwood@tadl.org>
2014-08-28 14:06:12 -0400

    this was throwing errors in phonegap
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- www/js/app.js
+++ www/js/app.js
@@ -4,9 +4,9 @@ var app = angular.module('egmobile', ['ionic','ngFitText'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+/*    if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
+    } */
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
